7b809b8f9d0033ebd4119ed9d27b329d4cd0e78f
Maarten van den Berg <maarten@channable.com>
2021-10-21 10:42:40 +0200

    Filter out non-code from Nix build

--- dbcritic.nix
+++ dbcritic.nix
@@ -2,7 +2,22 @@
 stdenv.mkDerivation {
     name = "dbcritic";
 
-    src = lib.cleanSource ./.;
+    # Remove files that don't affect the final build.
+    # Keep only:
+    #  - The Makefile
+    #  - Any file ending in `.idr`
+    #  - The directories containing source (i.e. not `.git`, `.semaphore` and `nix`)
+    src = lib.cleanSourceWith {
+      src = ./.;
+      filter = path: _type:
+        lib.strings.hasSuffix "Makefile" path
+        || lib.strings.hasSuffix ".idr" path
+        || (
+          lib.pathIsDirectory path
+          && !builtins.elem (baseNameOf path) ["nix" ".semaphore" ".git"]
+        );
+    };
+
     buildInputs = [ idris postgresql ];
 
     phases = [ "unpackPhase" "buildPhase"
